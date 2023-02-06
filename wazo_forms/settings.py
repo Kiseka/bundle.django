d2730b2489f3e1760dcb7a25977ab9345403fad7
Marvin Kiseka <kisekamarvin@gmail.com>
2023-02-06 10:52:50 +0300

    add git ignore

--- wazo_forms/settings.py
+++ wazo_forms/settings.py
@@ -12,6 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-c91*en9aw^d(=bfad#=)@t$2y2%ue+z)f=1qcxcbccv_hgedw%'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
